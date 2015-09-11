ce3f161056dfae9a1be6eab4e5fa0dc4f2c7cf35
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-11 13:27:26 -0600

    Seed file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -109,8 +109,8 @@ class Seed
   def run
     create_known_users
     create_categories
-    create_borrowers(30000)
     create_lenders(200000)
+    create_borrowers(30000)
     create_orders
   end
 
@@ -153,11 +153,12 @@ class Seed
         lr.user_id = user.id
         lr.title = Faker::Commerce.product_name
         lr.description = Faker::Company.catch_phrase
-        lr.amount = "200"
+        lr.amount = 200
         lr.status = [0, 1].sample
         lr.requested_by_date = Faker::Time.between(7.days.ago, 3.days.ago)
-        lr.contributed = "0"
-        lr.repayment_rate = "weekly"
+        lr.contributed = 0
+        lr.repayed = 0
+        lr.repayment_rate = [0, 1].sample
         lr.repayment_begin_date = Faker::Time.between(3.days.ago, Time.now)
 
         LoanRequestsCategory.populate(1) do |lrc|
@@ -177,9 +178,7 @@ class Seed
 
   def create_orders
     loan_requests = LoanRequest.all.sample(50000)
-
     possible_donations = %w(25, 50, 75, 100, 125, 150, 175, 200)
-
     loan_requests.each do |request|
       donate = possible_donations.sample
       lender = lenders.sample(1)
@@ -187,12 +186,108 @@ class Seed
                                {"#{request.id}" => donate},
                            user_id: lender.id)
       order.update_contributed(lender)
-
       puts "Created Order for Request #{request.title} by Lender #{lender.name}"
     end
-
   end
 
 end
 
 Seed.new.run
+
+
+# require "populator"
+# class Seed
+#   def run
+#     create_known_users
+#     create_borrowers(30000)
+#     create_lenders(200000)
+#     create_categories
+#     create_loan_requests_for_each_borrower(500000)
+#     create_orders(50000)
+#   end
+#
+#   def lenders
+#     User.where(role: 0)
+#   end
+#
+#   def borrowers
+#     User.where(role: 1)
+#   end
+#
+#   def orders
+#     Order.all
+#   end
+#
+#   def create_known_users
+#     User.create(name: "Jorge", email: "jorge@example.com", password: "password")
+#     User.create(name: "Rachel", email: "rachel@example.com", password: "password")
+#     User.create(name: "Josh", email: "josh@example.com", password: "password", role: 1)
+#   end
+#
+#   def create_lenders(quantity)
+#     User.populate(quantity) do |user|
+#       user.name = Faker::Name.name
+#       user.email = Faker::Internet.email
+#       user.password_digest = "password"
+#       user.role = 0
+#     end
+#   end
+#
+#   def create_borrowers(quantity)
+#     User.populate(quantity) do |user|
+#       user.name = Faker::Name.name
+#       user.email = Faker::Internet.email
+#       user.password_digest = "password"
+#       user.role = 1
+#     end
+#   end
+#
+#   def create_categories
+#     ["agriculture", "community", "education", "pizza", "dolphin", "cat", "dog", "monkey", "pony", "rhino", "whale", "asdf", "haha", "lol", "rofl"].each do |cat|
+#       Category.create(title: cat, description: cat + " stuff")
+#     end
+#   end
+#
+#   # def put_requests_in_categories
+#   #   categories = Category.all
+#   #
+#   #   LoanRequest.all.each do |request|
+#   #     categories.sample(1).loan_requests << request
+#   #   end
+#   # end
+#   def create_loan_requests_for_each_borrower(quantity)
+#     b = borrowers
+#     categories = Category.all
+#     LoanRequest.populate(quantity) do |loan_request|
+#       loan_request.title = Faker::Commerce.product_name
+#       loan_request.description = Faker::Company.catch_phrase
+#       loan_request.status = [0, 1].sample
+#       loan_request.repayment_begin_date = Faker::Time.between(3.days.ago, Time.now)
+#       loan_request.requested_by_date = Faker::Time.between(7.days.ago, 3.days.ago)
+#       loan_request.contributed = "0"
+#       loan_request.repayment_rate = 1
+#       loan_request.amount = 200
+#       loan_request.user_id = b.sample.id
+#       LoanRequestsCategory.populate(1) do |lrc|
+#         lrc.category_id = categories.sample.id
+#         lrc.loan_request_id = loan_request.id
+#       end
+#     end
+#   end
+#
+#   def create_orders(quantity)
+#     possible_donations = %w(25 50 75 100 125 150 175 200)
+#     loan_request_ids = (1..500000).to_a
+#     lndrs = lenders
+#     quantity.times do
+#       donation = possible_donations.sample
+#       lender = lndrs.sample
+#       request_id = loan_request_ids.sample
+#       order = Order.create(cart_items: {"#{request_id}" => donation},
+#                            user_id: lender.id)
+#       order.update_contributed(lender)
+#       puts "Created order ##{order.id}"
+#     end
+#   end
+# end
+# Seed.new.run
