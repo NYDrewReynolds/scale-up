11ab639991729ea9b46d8ea1fca2cbd78c5ed4b1
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-15 15:38:05 -0600

    More load scripts & threading

--- lib/load_script/session.rb
+++ lib/load_script/session.rb
@@ -10,6 +10,7 @@ module LoadScript
   class Session
     include Capybara::DSL
     attr_reader :host
+
     def initialize(host = nil)
       Capybara.default_driver = :poltergeist
       @host = host || "http://localhost:3000"
@@ -46,17 +47,16 @@ module LoadScript
     end
 
     def actions
-      # [:browse_loan_requests, :sign_up_as_lender]
-      [:browse_loan_requests]
+      [:browse_loan_requests, :user_browses_loan_requests, :user_browses_single_loan_request, :sign_up_as_lender, :sign_up_as_borrower]
     end
 
-    def log_in(email="demo+horace@jumpstartlab.com", pw="password")
+    def log_in(email="jorge@example.com", pw="password")
       log_out
       session.visit host
-      session.click_link("Log In")
-      session.fill_in("email_address", with: email)
-      session.fill_in("password", with: pw)
-      session.click_link_or_button("Login")
+      session.click_link("Login")
+      session.fill_in("Email", with: email)
+      session.fill_in("Password", with: pw)
+      session.click_link_or_button("Log In")
     end
 
     def browse_loan_requests
@@ -81,20 +81,54 @@ module LoadScript
     end
 
     def sign_up_as_lender(name = new_user_name)
+      puts "sign up as lender"
       log_out
       session.find("#sign-up-dropdown").click
       session.find("#sign-up-as-lender").click
-      session.within("#lenderSignUpModal") do
-        session.fill_in("user_name", with: name)
-        session.fill_in("user_email", with: new_user_email(name))
-        session.fill_in("user_password", with: "password")
-        session.fill_in("user_password_confirmation", with: "password")
-        session.click_link_or_button "Create Account"
-      end
+      session.fill_in("user_name", with: name)
+      session.fill_in("user_email", with: new_user_email(name))
+      session.fill_in("user_password", with: "password")
+      session.fill_in("user_password_confirmation", with: "password")
+      session.click_link_or_button "Create Account"
     end
 
     def categories
       ["Agriculture", "Education", "Community"]
     end
+
+    def user_browses_loan_requests
+      log_in
+      session.visit "#{host}/browse"
+      puts "user is browsing loan requests"
+      session.find('.next_page').click
+    end
+
+    def user_browses_single_loan_request
+      puts "user is browsing single loan request"
+      log_in
+      session.visit "#{host}/#{LoanRequest.all.limit(1).first.id}"
+    end
+
+    def sign_up_as_borrower(name = new_user_name)
+      puts "sign up as borrower"
+      log_out
+      session.find("#sign-up-dropdown").click
+      session.find("#sign-up-as-borrower").click
+      session.fill_in("user_name", with: name)
+      session.fill_in("user_email", with: new_user_email(name))
+      session.fill_in("user_password", with: "password")
+      session.fill_in("user_password_confirmation", with: "password")
+      session.click_link_or_button "Create Account"
+    end
+    #[X] Anonymous user browses loan requests
+    #[X] User browses pages of loan requests
+    #[] User browses categories
+    #[] User browses pages of categories
+    #[X] User views individual loan request
+    #[X] New user signs up as lender
+    #[X] New user signs up as borrower
+    #[] New borrower creates loan request
+    #[] Lender makes loan
+
   end
 end
